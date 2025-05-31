ed5f2e9522dc73fd08a715769553f4aa5f1cd236
MuhammedEmin <muhammedeminpakoz@gmail.com>
2025-05-31 19:26:47 +0300

    a
    a

--- han-game/Scripts/enemy.gd
+++ han-game/Scripts/enemy.gd
@@ -1,10 +1,21 @@
 extends CharacterBody2D
 
 
-var player
+@onready var player = get_node("/root/game/Hancı") 
+@onready var animation_player = $AnimatedSprite2D
 
 func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
-	velocity = direction * 300
+	velocity = direction * 100
+	
+	var distance = sqrt(pow(global_position.x - player.global_position.x,2)+pow(global_position.y - player.global_position.y,2))
+	
+	if(distance <= 25.0):
+		velocity = Vector2(0,0)
+		attack()
+	
 	move_and_slide()
 	
+	
+func attack():
+	animation_player.play("down_preparation")
